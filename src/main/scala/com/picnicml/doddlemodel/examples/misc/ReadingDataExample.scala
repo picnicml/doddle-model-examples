99b0e018e4542fdcb870013f9ee3459322d91d48
inejc <nejc.ilenic@gmail.com>
2018-05-25 13:12:19 +0200

    fix read data example

--- src/main/scala/com/picnicml/doddlemodel/examples/misc/ReadingDataExample.scala
+++ src/main/scala/com/picnicml/doddlemodel/examples/misc/ReadingDataExample.scala
@@ -1,15 +1,13 @@
 package com.picnicml.doddlemodel.examples.misc
 
-import java.io.File
-
-import breeze.linalg.csvread
+import com.picnicml.doddlemodel.data.loadCsvDataset
 
 object ReadingDataExample extends App {
   val dataDir = "/path/to/local/data/dir"
 
-  // assume a header line
-  val dataTr = csvread(new File(s"$dataDir/train.csv"), skipLines = 1)
-  val dataTe = csvread(new File(s"$dataDir/test.csv"), skipLines = 1)
+  // by default a header line is assumed
+  val dataTr = loadCsvDataset(s"$dataDir/train.csv")
+  val dataTe = loadCsvDataset(s"$dataDir/test.csv")
 
   // assume that 'label' is the first column
   val (xTr, yTr) = (dataTr(::, 1 to -1), dataTr(::, 0))
