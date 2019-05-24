f708e8c6a5b0f0a73f8da7778bffc6c61b78d9a3
inejc <nejc.ilenic@gmail.com>
2019-05-24 12:41:17 +0200

    use latest beta version

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,12 +3,14 @@ import sbt._
 object Dependencies {
 
   object DependencyVersion {
-    val doodleVersion = "0.0.1-beta2"
+    val doodleVersion = "0.0.1-beta3"
+    val breezeVersion = "0.13.2"
     val slf4jVersion = "1.7.26"
   }
 
   val compileDependencies: Seq[ModuleID] = Seq(
     "io.github.picnicml" %%  "doddle-model" % DependencyVersion.doodleVersion,
+    "org.scalanlp" %% "breeze-natives" % DependencyVersion.breezeVersion,
     "org.slf4j" % "slf4j-nop" % DependencyVersion.slf4jVersion
   )
 
