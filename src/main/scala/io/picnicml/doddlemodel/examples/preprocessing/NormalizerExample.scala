2688e23bad93fff235f1ebfef5295997c8563a25
inejc <nejc.ilenic@gmail.com>
2019-09-18 18:27:55 +0200

    add examples for recently added preprocessors

---
+++ src/main/scala/io/picnicml/doddlemodel/examples/preprocessing/NormalizerExample.scala
@@ -0,0 +1,6 @@
+package io.picnicml.doddlemodel.examples.preprocessing
+
+//import io.picnicml.doddlemodel.preprocessing.Normalizer
+
+object NormalizerExample extends App {
+}
---
+++ src/main/scala/io/picnicml/doddlemodel/examples/preprocessing/RangeScalerExample.scala
@@ -0,0 +1,6 @@
+package io.picnicml.doddlemodel.examples.preprocessing
+
+//import io.picnicml.doddlemodel.preprocessing.RangeScaler
+
+object RangeScalerExample extends App {
+}
