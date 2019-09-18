244d188b3056a50ba20d3be9f36f48d1ab65d9bb
inejc <nejc.ilenic@gmail.com>
2019-09-18 21:17:18 +0200

    add examples for recently added preprocessors

--- src/main/scala/io/picnicml/doddlemodel/examples/preprocessing/NormalizerExample.scala
+++ src/main/scala/io/picnicml/doddlemodel/examples/preprocessing/NormalizerExample.scala
@@ -1,6 +1,18 @@
 package io.picnicml.doddlemodel.examples.preprocessing
 
-//import io.picnicml.doddlemodel.preprocessing.Normalizer
+import breeze.linalg.DenseMatrix
+import io.picnicml.doddlemodel.preprocessing.Normalizer
+import io.picnicml.doddlemodel.preprocessing.Norms.L2Norm
+import io.picnicml.doddlemodel.syntax.TransformerSyntax._
 
 object NormalizerExample extends App {
+  val x = DenseMatrix(
+    List(1.0, 1.0, 1.0),
+    List(3.0, 0.0, 1.5),
+    List(6.0, 2.0, 0.0)
+  )
+
+  val normalizer = Normalizer(L2Norm)
+  val fittedNormalizer = normalizer.fit(x)
+  println(s"normalized rows:\n${fittedNormalizer.transform(x)}")
 }
